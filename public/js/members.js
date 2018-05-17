8ab2cbd642bc5aebd3d5067a635fbd4574fa4aff
Samuel Falcon <saxclar1025@gmail.com>
2018-05-16 23:22:38 -0400

    updated config, members page

--- public/js/members.js
+++ public/js/members.js
@@ -6,20 +6,53 @@ $(document).ready(function() {
         $.get("/api/users/"+id)
         .then(user=>{
             $(".member-name").text(user.firstName + " " + user.lastName);
+            var groupsCovered = [];
             $.get("/api/users/"+id+"/roles")
             .then(roles=>{
                 roles.forEach(role=>{
-                    console.log(role.name);
                     $.get("/api/roles/"+role.id+"/groups")
                     .then(groups=>{
                         groups.forEach(group=>{
                             //handle each group here
-                            console.log(group.name);
+                            if(groupsCovered.indexOf(group.id) != -1) {
+                                return;
+                            }
+                            groupsCovered.push(group.id);
+                            var $newGroupBody = $("<tbody id='group" + group.id + "Body'>");
+                            $("<table>")
+                                .addClass("mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp")
+                                .append(
+                                    $("<thead><tr><th><a data-toggle='collapse' href='#group" + group.id + "Body'>"+group.name+"</th></tr></thead>")
+                                )
+                                .append($newGroupBody)
+                                .appendTo($("#survey-container"));
                             $.get("/api/groups/"+group.id+"/tasks")
                             .then(tasks=>{
                                 tasks.forEach(task=>{
                                     //handle each task here
-                                    console.log(task.name);
+                                    var $newTask = $("<tr>");
+                                    $newTask.append($("<td>"+task.name+"</td>"))
+                                        .append($("<td>$"+task.commission+"</td>"))
+                                        .append($("<td>")
+                                            .append("<input type='number' value='0' min='0' step = '1' id='task"+ task.id + "qty'>")
+                                            .on("change", event=>{
+                                                //update entry for that event
+                                            })
+                                        )
+                                        .append($("<td>")
+                                            .append($("<button>+</button>").click(event=>{
+                                                $("#task"+task.id+"qty").val(parseInt($("#task"+task.id+"qty").val())+1);
+                                                })
+                                            )
+                                        )
+                                        .append($("<td>")
+                                            .append($("<button>-</button>").click(event=>{
+                                                if($("#task"+task.id+"qty").val()<1) return;
+                                                $("#task"+task.id+"qty").val(parseInt($("#task"+task.id+"qty").val())-1);
+                                                })
+                                            )
+                                        );
+                                    $newTask.appendTo($newGroupBody);
                                 });
                             });
                         });
--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -41,7 +41,7 @@ module.exports = function(app) {
     });
 
     // Route for logging user out
-    app.get("/logout", function(req, res) {
+    app.get("/api/logout", function(req, res) {
         req.logout();
         res.redirect("/");
     });
