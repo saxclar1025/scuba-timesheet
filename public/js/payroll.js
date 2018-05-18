729e9f911fadf7616a6ad9d093d1047ba96b1b0d
Samuel Falcon <saxclar1025@gmail.com>
2018-05-18 04:00:15 -0400

    fixed logic for payroll page

--- public/js/payroll.js
+++ public/js/payroll.js
@@ -1,6 +1,6 @@
 function getWorkWeek(date,weekDiff) {
     if(!weekDiff) weekDiff=0;
-    var startMoment = moment(date).subtract(((moment().day()+6) % 7) - (7*weekDiff), "days"); //gets the latest monday + the week differential
+    var startMoment = moment(date).subtract(((date.day()+6) % 7) - (7*weekDiff), "days"); //gets the latest monday + the week differential
     var endMoment = moment(startMoment).add(6,"days");
     return {start:startMoment,end:endMoment};
 }
@@ -18,11 +18,18 @@ $(document).ready(function(){
     $("#start-date").val(startWeek.start.format("YYYY-MM-DD"));
     $("#end-date").val(startWeek.end.format("YYYY-MM-DD"));
     
-    $.get("/api/users")
-    .then(users=>{
-        users.forEach(user=>{
-            $("#user-select").append("<option value='" + user.id + "'>" + user.firstName + " " + user.lastName + "</option>");
-        });
+    $("#prev-week-button").click(event=>{
+        var newWeek = getWorkWeek(moment($("#start-date").val(), "YYYY-MM-DD"),(-1));
+        $("#start-date").val(newWeek.start.format("YYYY-MM-DD"));
+        $("#end-date").val(newWeek.end.format("YYYY-MM-DD"));
+        $("#start-date").trigger("change");
+    });
+
+    $("#next-week-button").click(event=>{
+        var newWeek = getWorkWeek(moment($("#start-date").val(), "YYYY-MM-DD"),1);
+        $("#start-date").val(newWeek.start.format("YYYY-MM-DD"));
+        $("#end-date").val(newWeek.end.format("YYYY-MM-DD"));
+        $("#start-date").trigger("change");
     });
 
     $("#user-select, #start-date, #end-date").on("change", event=>{
@@ -42,5 +49,13 @@ $(document).ready(function(){
                 });
             });
         });
+    });
+
+    $.get("/api/users")
+    .then(users=>{
+        users.forEach(user=>{
+            $("#user-select").append("<option value='" + user.id + "'>" + user.firstName + " " + user.lastName + "</option>");
+        });
+        $("#user-select").trigger("change");
     });
 });
