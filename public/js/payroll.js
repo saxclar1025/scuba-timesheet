16febda0f2e7ed173b0a9c74a5fd81f428cf6488
Samuel Falcon <saxclar1025@gmail.com>
2018-05-17 21:35:07 -0400

    completed payroll page (minus formatting), fixed entry api routes

---
+++ public/js/payroll.js
@@ -0,0 +1,46 @@
+function getWorkWeek(date,weekDiff) {
+    if(!weekDiff) weekDiff=0;
+    var startMoment = moment(date).subtract(((moment().day()+6) % 7) - (7*weekDiff), "days"); //gets the latest monday + the week differential
+    var endMoment = moment(startMoment).add(6,"days");
+    return {start:startMoment,end:endMoment};
+}
+
+$(document).ready(function(){
+    $.get("/api/currentuserid")
+    .then(id=>{
+        $.get("/api/users/"+id)
+        .then(user=>{
+            $("#username").text(user.firstName + " " + user.lastName);
+        });
+    });
+
+    var startWeek = getWorkWeek(moment());
+    $("#start-date").val(startWeek.start.format("YYYY-MM-DD"));
+    $("#end-date").val(startWeek.end.format("YYYY-MM-DD"));
+    
+    $.get("/api/users")
+    .then(users=>{
+        users.forEach(user=>{
+            $("#user-select").append("<option value='" + user.id + "'>" + user.firstName + " " + user.lastName + "</option>");
+        });
+    });
+
+    $("#user-select, #start-date, #end-date").on("change", event=>{
+        $("tbody").empty();
+        $.get("/api/entries/user/"+$("#user-select").val()+"/daterange/" + $("#start-date").val() + "/" + $("#end-date").val())
+        .then(entries=>{
+            entries.forEach(entry=>{
+                $.get("/api/tasks/" + entry.TaskId)
+                .then(task=>{
+                    var $entryRow = $("<tr>");
+                    $entryRow.append($("<td>").text(entry.date))
+                        .append($("<td>").text(task.name))
+                        .append($("<td>").text(task.commission))
+                        .append($("<td>").text(entry.quantity))
+                        .append($("<td>").text(parseInt(entry.quantity)*parseFloat(task.commission)));
+                    $entryRow.appendTo("tbody");
+                });
+            });
+        });
+    });
+});
--- routes/entry-api-routes.js
+++ routes/entry-api-routes.js
@@ -36,6 +36,13 @@ module.exports = function(app) {
         });
     });
 
+    app.get("/api/entries/user/:userid/daterange/:startdate/:enddate", (req,res)=>{
+        db.Entry.findAll({where:{date:{between: [req.params.startdate,req.params.enddate]},UserId:req.params.userid}})
+        .then(entries=>{
+            res.json(entries);
+        });
+    });
+
     app.post("/api/entries", (req,res)=>{
         console.log("Entry posted");
         console.log(res.body);
@@ -77,6 +84,15 @@ module.exports = function(app) {
         db.Entry.destroy({where:{id:req.params.id}})
         .then(entry=>{
             res.json(entry);
+        });
+    });
+
+    app.get("/api/entries/:id/total", (req,res)=>{
+        db.Entry.findById(req.params.id)
+        .then(entry=>{
+            db.Task.findById(entry.TaskId).then(task=>{
+                res.json(parseInt(entry.quantity)*parseFloat(task.commission));
+            });
         });
     });
 }
